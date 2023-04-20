3e9a289d638d4ad39b5ef22d88c899d94982e6e9
hoangkim <hoangkim@students.uni-marburg.de>
2023-04-20 14:59:16 +0000

    Hoangkim add Maven, readCSV.java

---
+++ src/main/java/readCSV.java
@@ -0,0 +1,39 @@
+import com.opencsv.CSVReader;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class readCSV {
+
+    public static List<String[]> readCSV(File csvFile) throws Exception {
+        CSVReader reader = new CSVReader(new FileReader(csvFile));
+        List<String[]> list = reader.readAll();
+
+        return list;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        // Create a file chooser dialog to select the CSV file
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(null);
+        if (result != JFileChooser.APPROVE_OPTION) {
+            return; // User canceled or closed the dialog
+        }
+        File selectedFile = fileChooser.getSelectedFile();
+
+        //String csvFile = "teilnehmerliste.csv";
+
+        List<String[]> list = readCSV(selectedFile);
+
+        //print out to the console the list of participant from csv file
+        for (String[] teilnehmer : list) {
+            System.out.println(Arrays.toString(teilnehmer));
+        }
+
+
+    }
+}
